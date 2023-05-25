9d3520101bd1794ed088d3e6bf4af2e1924533ce
@ivan_okhlopkov <iokhlopkov@gmail.com>
2023-05-25 14:55:46 +0300

    done StringsAndRegex

--- src/main/java/practice/regex/SplitText.java
+++ src/main/java/practice/regex/SplitText.java
@@ -2,13 +2,22 @@ package practice.regex;
 
 public class SplitText {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
+    }
 
-  public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    return "";
-  }
+    public static String splitTextIntoWords(String text) {
+        //TODO реализуйте метод
+        String[] words = text.replaceAll("[.,0-9;`]", "").split("\s+");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            if (i == words.length - 1) {
+                result.append(words[i]);
+                break;
+            }
+            result.append(words[i]).append("\n");
+        }
+        return result.toString();
+    }
 
 }
--- src/main/java/practice/string/SequentialWordsNumbers.java
+++ src/main/java/practice/string/SequentialWordsNumbers.java
@@ -2,7 +2,21 @@ package practice.string;
 
 public class SequentialWordsNumbers {
 
-    public static String sequentialWordsNumbers(String text){
-        return "";
+    public static String sequentialWordsNumbers(String text) {
+        int start = 0;
+        int wordNumber = 1;
+        StringBuilder result = new StringBuilder();
+        while (start < text.length()) {
+            int end = text.indexOf(' ', start);
+            if (end < 0) {
+                end = text.length();
+            }
+            String test = text.substring(start, end);
+            result.append(" (").append(wordNumber).append(") ").append(test);
+            start = end + 1;
+            wordNumber++;
+        }
+
+        return result.toString().strip();
     }
 }
